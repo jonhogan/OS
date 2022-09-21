e2d5c41977e34e58258130c3a057a5d9dbb0dbce
Jon <jon.hogan83@gmail.com>
2022-09-20 20:04:30 -0500

    Update Mini Project

--- mp1/main.c
+++ mp1/main.c
@@ -5,16 +5,33 @@
 #define myPID getpid()
 #define parentID getppid()
 
-void *read_chars(void *pid){
+void *read_chars(void *pid, char $array){
   
 }
 
 int main(int argc, char *argv[]){
-  int  pid;
+  int pid;
+  int i = 1;
   char *charArray;
 
   charArray = malloc(60);
 
+  //Open and read from the file
+  FILE *doc;
+
+  doc = fopen("text.dat", "r");
+
+  if(NULL == doc){printf("File not found.\n")};
+
+  charArray[0] = fgetc(doc);
+
+  while(doc != EOF){
+    charArray[i] = fgetc(doc);
+  }
+
+  for(int j = 0; j < 60; j++){
+    printf("%c ", charArray[j]);
+  }
 
  //Create the two child processes 
   fork();
